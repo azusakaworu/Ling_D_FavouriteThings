8bf3965a1388b8ce63abce438ddb8c7290a879e5
Ling Ding <azusakaworu@gmail.com>
2019-03-26 22:22:18 -0400

    function

--- routes/index.js
+++ routes/index.js
@@ -8,26 +8,26 @@ var connect = require('../utils/sqlConnect');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //do a database query  name avatar 
-  connect.query(`SELECT name, avatar FROM hero`,(err, result)=>{
+  connect.query(`SELECT fave_title, fave_img FROM tbl_fave`,(err, result)=>{
     if(err){
       throw err;
       console.log(err);
     }else{console.log(result);
-      res.render('index', { avatars: result });
+      res.render('index', { faves: result });
     }
   });
   
 });
 
 //get indiividual data /bio info :hero -->placeholder
-router.get('/:hero', function(req, res, next) {
+router.get('/:anime', function(req, res, next) {
   
-  connect.query(`SELECT * FROM hero WHERE name="${req.params.hero}"`,(err, result)=>{
+  connect.query(`SELECT * FROM tbl_fave WHERE fave_title="${req.params.anime}"`,(err, result)=>{
     if(err){
       throw err;
       console.log(err);
     }else{console.log(result);
-      res.render('bio', { bioData: result[0]});
+      res.render('detail', { detailData: result[0]});
     }
   });
   
